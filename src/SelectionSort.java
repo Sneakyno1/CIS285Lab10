3ecb0ed85eb740b0bb083a9c6fc5e275114b3472
Andrew <ajingles@umich.edu>
2022-12-01 13:54:25 -0500

    Fixed issues in Selection sort for mixed value arrays, and arrays with duplicate elements

--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -13,13 +13,18 @@ public class SelectionSort {
      */
     public int[] basicSelectionSort(int[] x) {
         for (int i = 0; i < x.length; ++i) {
+            int currMin = x[i];
+            int replacementIndex = i;
             for (int j= i+1; j < x.length; ++j) {
-                if (x[i] > x[j]) {
-                    temp = x[i];
-                    x[i] = x[j];
-                    temp = x[j];
+                if (x[j] < currMin) {
+                    currMin = x[j];
+                    replacementIndex = j;
                 }
             } // end of inner for loop
+            int temp = x[i];
+            x[i] = x[replacementIndex];
+            x[replacementIndex] = temp;
+
         } // end of outer for loop
         return x;
     } // end of basicSelectionSort method
--- src/testSelectionSort.java
+++ src/testSelectionSort.java
@@ -1,5 +1,6 @@
 import org.junit.Test;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 public class testSelectionSort {
@@ -14,11 +15,45 @@ public class testSelectionSort {
 
     public void testMixed(){
 
-        /** Test data contains with both positive, negative and zeros **/
+        int[] arr = new int[5];
+        arr[0] = 8;
+        arr[1] = -9;
+        arr[2] = 7;
+        arr[3] = -10;
+        arr[4] = -2;
+
+        int[] Sortedarr = new int[5];
+        Sortedarr[0] = -10;
+        Sortedarr[1] = -9;
+        Sortedarr[2] = -2;
+        Sortedarr[3] = 7;
+        Sortedarr[4] = 8;
+
+        SelectionSort temp = new SelectionSort();
+        temp.basicSelectionSort(arr);
+
+        assertArrayEquals( "array not sorted", Sortedarr, arr);
     }
 
     public void testDuplicates(){
 
-        /** Test data contains duplicates **/
+        int[] arr = new int[5];
+        arr[0] = 10;
+        arr[1] = 9;
+        arr[2] = 2;
+        arr[3] = 10;
+        arr[4] = 2;
+
+        int[] Sortedarr = new int[5];
+        Sortedarr[0] = 2;
+        Sortedarr[1] = 2;
+        Sortedarr[2] = 9;
+        Sortedarr[3] = 10;
+        Sortedarr[4] = 10;
+
+        SelectionSort temp = new SelectionSort();
+        temp.basicSelectionSort(arr);
+
+        assertArrayEquals( "array not sorted", Sortedarr, arr);
     }
 }
